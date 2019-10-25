991db11f5ee44c7031512c185db9d8168048c43e
System Administrator <root@Alessandros-iMac.local>
2019-10-26 01:00:40 +0200

    rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { LiteralMapEntry } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -8,28 +9,103 @@ import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms'
 })
 export class AppComponent  implements OnInit{
   formGroup:FormGroup;
-  disabled:boolean;
+  wrongTest1:boolean;
+  wrongTest2:boolean;
+  wrongTest3:boolean;
+  wrongPin:boolean;
+  dd = '26';
+  mm = '07';
+  aa = '82';
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(){
-    this.disabled = true;
+
     this.formGroup = new FormGroup({
-      'input': new FormControl(null)
+      'input': new FormControl(null),
+      'pin': new FormControl(null)
 
     })
+    this.formGroup.get('pin').disable();
   }
 
-    onSubmit(data){
-      let re = /(0123|1234|2345|3456|4567|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|3210)+/ig;
-      let test  = re.test(data.input);
-      if (test===true){
-       this.formGroup.controls['input'].setErrors({'incorrect': true});
+    control(data){
+      this.wrongTest1 = null;
+      this.wrongTest2 = null;
+      this.wrongTest3 = null;
+      let dim = data.input.length;
+      this.formGroup.get('pin').disable();
+
+      if (dim===6){
+        let re = /(0123|1234|2345|3456|4567|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|3210)+/ig;
+        let re1 = /(0000|1111|2222|3333|4444|5555|6666|7777|8888|9999)+/ig;
+        let re2 = this.dd+this.mm+this.aa;
+        let re3 = this.dd+this.aa+this.mm;
+        let re4 = this.mm+this.aa+this.dd;
+        let re5 = this.mm+this.dd+this.aa;
+        let re6 = this.aa+this.dd+this.mm;
+        let re7 = this.aa+this.mm+this.dd;
+        let test  = re.test(data.input);
+        let test1  = re1.test(data.input);
+        let test2  = data.input.includes(re2) || data.input.includes(re3) || data.input.includes(re4) || data.input.includes(re5) || data.input.includes(re6) || data.input.includes(re7);
+
+        if (test===true){
+         this.formGroup.controls['input'].setErrors({'incorrect': true});
+         this.wrongTest1 = true;
+         return false;
+        }
+        else
+        {
+          this.wrongTest1 = false;
+        }
+        if (test1===true){
+          this.formGroup.controls['input'].setErrors({'incorrect': true});
+          this.wrongTest2 = true;
+          return false;
+         }
+         else
+         {
+           this.wrongTest2 = false;
+         }
+         if (test2===true){
+          this.formGroup.controls['input'].setErrors({'incorrect': true});
+          this.wrongTest3 = true;
+          return false;
+         }
+         else
+         {
+           this.wrongTest3 = false;
+
+         }
+         this.formGroup.get('pin').enable();
       }
-      else
+
+    }
+
+    controlPin(data){
+      this.wrongPin=null;
+      let dim = data.pin.length;
+     if(dim>0){
+
+      let pin = data.input;
+      pin = pin.split("");
+      let pinDown = data.pin;
+      pinDown = pinDown.split("");
+
+      for(let i=0; i<pinDown.length; i++){
+            if (pin[i]!==pinDown[i]){
+              this.wrongPin=true;
+              return false;
+            }
+      }
+      if (dim===6)
+        this.wrongPin=false;
+     }
+
+    }
+
+    onSubmit(data){
       alert('ok');
-
-
     }
 }
 
